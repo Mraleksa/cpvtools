91dd0f8a687205f38924f60e838aaae638b4cc42
Mraleksa <a.gribanovskiy@gmail.com>
2017-11-10 14:51:09 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ var sqlite3 = require("sqlite3").verbose();
 // Open a database handle
 var db = new sqlite3.Database("data.sqlite");
 
-var currentCount =  "2017-07-18T16:02:44.083417+03:00"
+var currentCount =  "2017-01-01T16:02:44.083417+03:00"
 var p=0; var p2=0;var description,status,cpv,name,winner,region,mail,edr,tenderID,amount;
  
 function piv(){  
@@ -76,7 +76,7 @@ statement.finalize();
 		
 		})
 		.then(function () {	
-		if (p<10){piv ();}		
+		if (p<3){piv ();}		
 		else {
 			console.log("stop")
 				p=0;
@@ -84,7 +84,7 @@ statement.finalize();
 				console.log(p2)
 			setTimeout(function() {
 			
-				if (p2 <500) {
+				if (p2 <3) {
 					piv ();
 				}
 				else {console.log("STOP")}
